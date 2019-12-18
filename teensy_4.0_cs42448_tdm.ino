1a39b0b2c2db09d15338a822250ce3fed098fc5e
William Hofferbert <will.hbert@gmail.com>
2019-12-17 23:46:58 -0500

    removed SingleNoteTracker guts

--- teensy_4.0_cs42448_tdm.ino
+++ teensy_4.0_cs42448_tdm.ino
@@ -19,6 +19,7 @@
 #include <vector>
 // https://github.com/wizard97/Embedded_RingBuf_CPP
 #include "RingBufCPP.h"
+#include "SingleNoteTracker.h"
 
 // best current fix for both of: 
 // undefined reference to `__exidx_end'
@@ -28,23 +29,6 @@ unsigned __exidx_end;
 // re: https://forum.pjrc.com/threads/57192-Teensy-4-0-linker-issues-with-STL-libraries
 // might break things that are already broken, but it's over my head
 
-// midi channel selection
-// is this 0 or 1 based?
-const int midiChannel = 1;
-
-// midi note/peak buffer lengths.
-// note: must be multiple of 2!
-// maybe make this 16? 24?
-const int noteRingBufferLength = 8;
-const int peakRingBufferLength = 8;
-
-// when to turn off notes because they fade out.
-const int midiMinimumVelocityThreshold = 2;
-
-// note frequency confidence factor
-// 0.15 = default. 0.11 = scrutiny
-const float confidenceThreshold = 0.11;
-
 // master timer
 unsigned long currentMillis = 0;
 
@@ -52,7 +36,7 @@ unsigned long previousLedMillis = 0;
 unsigned long intervalLedMillis = 2;
 
 unsigned long previousSerialMillis = 0;
-unsigned long intervalSerialMillis = 150;
+unsigned long intervalSerialMillis = 1000;
 
 //
 //
@@ -184,224 +168,6 @@ std::vector<AudioAnalyzeNoteFrequency *> freqs = {&notefreq1, &notefreq2, &notef
 std::vector<AudioAnalyzePeak *> peaks = {&peak1, &peak2, &peak3, &peak4, &peak5, &peak6};
 
 
-// bits of data to pass
-struct signalData {float freq, peak;};
-
-
-// TODO the following four funcs might be better off in teh single note tracker class
-// thanks, https://newt.phys.unsw.edu.au/jw/notes.html
-unsigned char freqToMidiNote(float freq) {
-  // 12 notes per scale, log base 2 (freq / reference freq) + ref freq midi number
-  // 12 * (f/440) + 69
-  unsigned int ret = roundf( 12.0 * log2f( freq / 440.0 ) ) + 69.0;
-  return ret;
-}
-
-
-unsigned char peakToMidiVelocity(float peak) {
-  // convert float to 0-127 scale
-  unsigned char ret = peak * 127.0;
-  return(ret);
-}
-
-
-void sendNoteOn(unsigned char note, unsigned char velocity) {
-  if (note > 127 || note < 0) {
-    return;
-  }
-  if (velocity > 127 || velocity < 0) {
-    return;
-  }
-  // TODO option for always full velocity?
-  // TODO velocity mod?
-  //Serial.printf("Would have sent note %d ON, vel %d\n", note, velocity);
-  usbMIDI.sendNoteOn(int(note), int(velocity), midiChannel);
-}
-
-
-void sendNoteOff(unsigned char note) {
-  if (note > 127 || note < 0) {
-    return;
-  }
-  //Serial.printf("Would have sent note %d OFF\n", note);
-  usbMIDI.sendNoteOff(note, 0, midiChannel);
-}
-
-
-// maybe investigate how to apply volume control/fade per active midi note, for decay
-// TODO this class is lengthy. perhaps should be in another .cpp and .h file
-
-// single string monitoring
-class SingleNoteTracker
-{
-public:
-  // ring buffers.
-  // array of previous amplitudes
-  unsigned char name;
-  bool noteIsOn = false;
-  bool turnNoteOff = false;
-  unsigned char currentNote = 0;
-  unsigned char newNote = 0;
-  unsigned char currentVel = 0;
-  unsigned char newVel = 0;
-  RingBufCPP<unsigned char, peakRingBufferLength> velRingBuf;
-  // array of previous midi notes
-  RingBufCPP<unsigned char, noteRingBufferLength> noteRingBuf;
-  // keep track of current pitch/freq/note?
-  RingBufCPP<float, noteRingBufferLength> freqRingBuf;
-  RingBufCPP<float, peakRingBufferLength> peakRingBuf;
-  AudioAnalyzeNoteFrequency * freqPointer;
-  AudioAnalyzePeak * peakPointer;
-
-  // member funcs;
-  void updateSignalData();
-  bool noteHasChanged();
-  bool amplitudeChanged();
-  bool hasAnythingChanged();
-  void stringSignalToMidi();
-};
-
-// sample the channel and add to ring buf
-// this seems to work now
-void SingleNoteTracker::updateSignalData() {
-  int lastFreqIndex = freqRingBuf.numElements(); 
-  int lastPeakIndex = peakRingBuf.numElements();
-  float lastFreq = *freqRingBuf.peek(freqRingBuf.numElements() - 1);
-  float lastPeak = *peakRingBuf.peek(peakRingBuf.numElements() - 1);
-  // buffer is getting filled properly
-  //Serial.printf("FreqIndex: %d\tPeakIndex: %d\n", lastFreqIndex, lastPeakIndex);
-  // lastFreq lastPeak are correct
-  //Serial.printf("Freq: %f\tPeak: %f\n", lastFreq, lastPeak);
-  signalData tmpData;
-  if (freqPointer->available() && peakPointer->available()) {
-    float note = freqPointer->read();
-    float peak = peakPointer->read();
-    //float prob = freqPointer->probability();
-    tmpData.freq = note;
-    tmpData.peak = peak;
-  //} else if (freqPointer->available() || peakPointer->available()) {
-  } else if (freqPointer->available()) {
-    // TODO there might be issues with this logic...
-    // like what happens when a signal goes away
-    // and the last signal we had was over the thresholds?
-    tmpData.freq = lastFreq;
-    tmpData.peak = lastPeak;
-  } else {
-    tmpData.freq = 0.0;
-    tmpData.peak = 0.0;
-  }
-
-  unsigned char midiNote = freqToMidiNote(tmpData.freq);
-  unsigned char midiVel = peakToMidiVelocity(tmpData.peak);
-
-  //if (name == 0) {
-  //  Serial.printf("Freq: %f\tPeak: %f\tMidi Note:%d\tVel: %d\n", tmpData.freq, tmpData.peak, midiNote, midiVel);
-  //}
-
-  // this has to be working because of the above peek logic working.
-  freqRingBuf.add(tmpData.freq, true);
-  peakRingBuf.add(tmpData.peak, true);
-  noteRingBuf.add(midiNote, true);
-  velRingBuf.add(midiVel, true);
-}
-
-
-// TODO funcs for comparing last data vs moving average in note buffer
-// this seems extra broken ...
-// if there's a new note (reliably), turn old note off, new note on... how to weight that properly
-bool SingleNoteTracker::noteHasChanged() {
-  int bufLen = noteRingBuf.numElements();
-  unsigned char total = 0;
-  for (int i=0; i< bufLen; i++) {
-    // notebuf
-    unsigned char noteVal = *noteRingBuf.peek(i);
-    //if (name == 0) {
-    //  Serial.printf("%d %d;\t", i, noteVal);
-    //}
-    total += noteVal;
-  }
-  unsigned char average = roundf(float(total) / bufLen);
-  //if (name == 0) {
-  //  Serial.printf("\ntotal: %d\tcurrentNote: %d\tBuffer Average: %d\n", total , currentNote, average);
-  //}
-  if (average != currentNote) {
-    newNote = average;
-    return(true);
-  } else {
-    return(false);
-  }
-}
-
-
-bool SingleNoteTracker::amplitudeChanged() {
-  bool changed = false;
-  int total;
-  for (int i=0; i<velRingBuf.numElements(); i++) {
-    total += *velRingBuf.peek(i);
-  }
-  // if the amplitude jumps (threshold diff up) back up but for the same note, then turn old note off and back on.
-  int average = roundf(float(total) / velRingBuf.numElements());
-  // TODO if amplitude jumps more than (percent? something?)
-  if (average > currentVel) {
-    newVel = average;
-    changed = true;
-  } else if (average <= midiMinimumVelocityThreshold) {
-    newVel = average;
-    turnNoteOff = true;
-    changed = true;
-  }
-
-  return(changed);
-}
-
-
-bool SingleNoteTracker::hasAnythingChanged() {
-  bool changed = false;
-  if (noteHasChanged()) {
-    changed = true;
-  } else if (amplitudeChanged()) {
-    changed = true;
-  }
-  return(changed);
-}
-
-
-void SingleNoteTracker::stringSignalToMidi() {
-  // TODO based on things, turn off/on notes
-  // TODO this might need to be different, like run every check func first, THEN step through note changes
-  // handle turning notes off...
-  bool noteWasTurnedOff = false;
-  if (newVel > currentVel || newNote != currentNote || turnNoteOff == true) {
-    // turn note off first
-    sendNoteOff(currentNote);
-    // mark that the note has been turned off
-    noteIsOn = false;
-    // mark that this fired?
-    noteWasTurnedOff = true;
-  }
-
-  bool noteWasChanged = false;
-  if (newNote != currentNote && noteIsOn == false) {
-    // TODO an override for max velocity every time?
-    // TODO sane to use newVel here?
-    sendNoteOn(newNote, newVel);
-    noteIsOn = true;
-    noteWasChanged = true;
-  } else if (newVel > currentVel && noteIsOn == false) {
-    sendNoteOn(currentNote, newVel);
-    noteIsOn = true;
-    noteWasChanged = true;
-  }
-
-  // mmmmm
-  if ( newNote != currentNote) {
-    currentNote = newNote;
-  }
-  if ( newVel != currentVel) {
-    currentVel = newVel;
-  }
-}
-
 // more global vars for the string trackers...
 SingleNoteTracker string1;
 SingleNoteTracker string2;
@@ -413,13 +179,12 @@ SingleNoteTracker string6;
 // vector for stringsNoteTracker pointers
 std::vector<SingleNoteTracker *> strings = {&string1, &string2, &string3, &string4, &string5, &string6};
 
-
 //
 void serialPrinter() {
   if (currentMillis - previousSerialMillis > intervalSerialMillis) {
     previousSerialMillis = currentMillis;
     // possibly serial print things here
-    //Serial.println("got here");
+    Serial.println("got here");
   }
 }
 
@@ -472,6 +237,8 @@ void setup() {
   Serial.println("got past string setup");
   delay(1000);
 }
+
+
 
 void loop() {
   // update timer for things
