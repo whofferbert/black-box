0913c2e0b140ec921864964d7d8cb3e668479699
William Hofferbert <will.hbert@gmail.com>
2019-12-18 19:54:53 -0500

    comments

--- Blinkenlights.h
+++ Blinkenlights.h
@@ -12,6 +12,7 @@
 struct rgbPins {int r, g, b;};
 struct rgbVals {int r, g, b;};
 
+// amount of cycles to wait on the main colors for
 const int colorIncrementDelay = 0;
 
 class rgbLED
@@ -34,10 +35,13 @@ public:
   void on();
 };
 
+// move through the spectrum
 void rgbLED::roygbiv_cycle();
 
+// AnalogWrite with the current values
 void rgbLED::on();
 
+// set values to colors
 void rgbLED::RED();
 void rgbLED::ORANGE();
 void rgbLED::YELLOW();
@@ -46,6 +50,7 @@ void rgbLED::BLUE();
 void rgbLED::INDIGO();
 void rgbLED::VIOLET();
 
+// called by roygbiv_cycle
 rgbVals rgbLED::cycleLedRGB (rgbVals v);
 
 
